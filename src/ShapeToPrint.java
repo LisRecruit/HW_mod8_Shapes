a5296c90cbc41767b6769c137676a687253587ba
Igor <igorlis159@gmail.com>
2024-05-22 16:02:43 +0300

    refactor

--- src/ShapeToPrint.java
+++ src/ShapeToPrint.java
@@ -1,11 +1,6 @@
 public class ShapeToPrint implements Printable{
-
-
-
     @Override
     public void print (Shape shape){
         System.out.println("Figure name is "+ shape.getName());
     }
-
-
 }
